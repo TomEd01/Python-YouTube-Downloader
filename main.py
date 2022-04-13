a0d4816e61a3a804ce64e0e0ec4ce4a87596514a
TomiWebsite <tomicatwebsite@gmail.com>
2022-04-13 14:27:08 -0500

    Arreglando el error al descargar el audio

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from pytube import YouTube #Libreria de youtube
 from colorama import * #Libreria de colores en python
+import os
 def completo(stream, filepath):
     print(Fore.MAGENTA + 'Ubicación \033 '+ filepath, Style.RESET_ALL)#Mostramos la ubicación del archivo y resetteamos los colores
 def cargando(stream, chunk, bytes_remaining):
     proceso = f'{round(100 - (bytes_remaining / stream.filesize * 100),2)}%' #Hacemos una operación para mostrar el porcentaje
     print('\n'+Fore.RED + proceso)
-#print(video.streams.get_highest_resolution())#Maxima resolucion
-#print(video.streams.get_lowest_resolution())#Media resolución
-#print(video.streams.get_audio_only())#Tipo audio               https://www.youtube.com/watch?v=C9qxnGlS3PQ
+
+#https://www.youtube.com/watch?v=C9qxnGlS3PQ
+
 init() #Iniciamos la libreria colorama
 ubicacion = r'C:\Users\TomiCat xD\Downloads' #Variable para la ruta, lo mejorare a una ventana ;)
 
@@ -35,7 +36,11 @@ while True: #Empezamos a iniciar un menu interactivo
         elif posible_error == 'b':
             video.streams.get_lowest_resolution().download(ubicacion)#De calidad media-baja
         elif posible_error == 's':
-            video.streams.get_audio_only().download(ubicacion)#Obción de audio mp3
+            audio = video.streams.filter(only_audio=True).first()#Filtramos el tipo audio
+            descarga = audio.download(ubicacion)#Obción de audio mp3
+            base, ext = os.path.splitext(descarga)#Preparamos para pasarlo de mp4 a mp3
+            nuevo = base + '.mp3'#Le cambiamos la extención
+            os.rename(descarga, nuevo)#Ya que descargamos lo convertimos a mp3
         elif posible_error == 'e':
             exit()#Para cancelar la consola
         pregunta = input('\n¿Quieres repetir? y/n: ') #Aqui empiza el menu interactivo
